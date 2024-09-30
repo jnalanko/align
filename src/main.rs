259a02edff0aa49f8d8afa616fbf083d0afd7f16
Jarno N. Alanko <alanko.jarno@gmail.com>
2024-09-30 23:06:00 +0300

    --length-only

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,19 @@ fn main(){
         .arg_required_else_help(true)
         .arg(Arg::new("fasta")
             .help("Input fasta file containing the two sequences to align.")
-            .required(true)
-            .index(1))
+            .required(true))
         .arg(Arg::new("columns")
+            .long("columns")
+            .short('c')
             .help("Number of columns to print.")
-            .required(true)
-            .index(2))
+            .default_value("80")
+        )
+        .arg(Arg::new("length-only")
+            .help("Print only the alignment length in each query. Only makes sense with local alignment.")
+            .long("length-only")
+            .conflicts_with("global")
+            .conflicts_with("columns")
+            .action(ArgAction::SetTrue))
         .arg(Arg::new("global")
             .short('g')
             .long("global")
@@ -23,6 +30,7 @@ fn main(){
     let infile = matches.get_one::<String>("fasta").unwrap();
     let n_columns = matches.get_one::<String>("columns").unwrap().parse::<usize>().unwrap();
     let global = matches.get_flag("global");
+    let length_only = matches.get_flag("length-only");
 
     let reader = jseqio::reader::DynamicFastXReader::from_file(&infile).unwrap();
     let db = reader.into_db().unwrap();
@@ -48,14 +56,21 @@ fn main(){
             true => aligner.global(x_rc, y),
             false => aligner.local(x_rc, y),
         };
-        
-        if alignment_rc.score > alignment.score {
-            eprintln!("Aligned reverse complement to forward");
-            eprintln!("{}", alignment_rc.pretty(x, y, n_columns));
+
+        if length_only {
+            let better_len = std::cmp::max(alignment.x_aln_len(), alignment_rc.x_aln_len());
+            println!("{}", better_len);
         }
         else {
-            eprintln!("Aligned forward to forward");
-            eprintln!("{}", alignment.pretty(x, y, n_columns));
+            #[allow(clippy::collapsible_else_if)] // Clearer this way
+            if alignment_rc.score > alignment.score {
+                println!("Aligned reverse complement to forward");
+                println!("{}", alignment_rc.pretty(x, y, n_columns));
+            }
+            else {
+                println!("Aligned forward to forward");
+                println!("{}", alignment.pretty(x, y, n_columns));
+            }
         }
     }
 
